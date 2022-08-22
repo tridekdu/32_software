85754caef5be13908f11c42604d4182351a0893f
32 <tridekdu@gmail.com>
2022-08-22 00:56:40 +0000

    kinda works

--- eyemimic.cpp
+++ eyemimic.cpp
@@ -6,6 +6,11 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <math.h>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include "ketopt.h"
 
@@ -29,7 +34,9 @@ float y = 0;
 float w = input_width;
 float h = input_height;
 float r = 0;
-float z = 0.5f;
+float q = 0.5f;
+float zx = 0.5f;
+float zy = 0.5f;
 float k1 = 0.5f;
 float k2 = 0.5f;
 float k3 = 0.5f;
@@ -39,6 +46,7 @@ float p2 = 0.5f;
 float cut = 0.5f;
 float d = 0.5f;
 float e = 0.5f;
+float c = 0.5f;
 
 // --------------------------------------------------
 
@@ -56,6 +64,8 @@ int pupil_max_y = input_height/2;
 cv::Rect roi_rect(x, y, w, h);
 
 cv::Mat cam_input;
+cv::Mat cam_gray;
+cv::Mat cam_remapped;
 cv::Mat cam_procbuf_1;
 cv::Mat cam_procbuf_2;
 cv::Mat cam_procbuf_3;
@@ -63,8 +73,8 @@ cv::Mat cam_procbuf_4;
 cv::Mat cam_procbuf_5;
 
 cv::Mat pupil(64, 64, CV_8UC3);
-cv::Mat eye_buffer_L(input_width, input_height, CV_32F, cv::Scalar(0));
-cv::Mat eye_buffer_R(input_width, input_height, CV_32F, cv::Scalar(0));
+cv::Mat eye_buffer_L(input_width, input_height, CV_32FC3, cv::Scalar(0));
+cv::Mat eye_buffer_R(input_width, input_height, CV_32FC3, cv::Scalar(0));
 //cv::Mat buffer_L(EMAT_W * 3, EMAT_H * 3, CV_8UC3);
 //cv::Mat buffer_R(EMAT_W * 3, EMAT_H * 3, CV_8UC3);
 
@@ -81,26 +91,12 @@ uint8_t* eoR = (uint8_t*)eye_out_R.data;
 cv::Size frameSize(cv::Size(input_width, input_height));
 cv::Mat cameraMatrix(3, 3, cv::DataType<float>::type);
 cv::Mat distCoeffs(5, 1, cv::DataType<float>::type);
-cv::Mat mapx, mapy, dilate_element;
+cv::Mat mapx, mapy, dilate_element, erode_element;
+
 
 
 // Setup SimpleBlobDetector parameters.
 cv::SimpleBlobDetector::Params params;
-// Change thresholds
-//params.minThreshold = 10;
-//params.maxThreshold = 200;
-// Filter by Area.
-//params.filterByArea = true;
-//params.minArea = 1500;
-// Filter by Circularity
-//params.filterByCircularity = true;
-//params.minCircularity = 0.1;
-// Filter by Convexity
-//params.filterByConvexity = true;
-//params.minConvexity = 0.87;
-// Filter by Inertia
-//params.filterByInertia = true;
-//params.minInertiaRatio = 0.01;
 
 std::vector<cv::KeyPoint> keypoints;
 cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
@@ -132,13 +128,13 @@ std::string gstreamer_pipeline(int width, int height, int framerate) {
     	" libcamerasrc ! video/x-raw, "
 		" width=(int)" + std::to_string(width) + ","
 		" height=(int)" + std::to_string(height) + ","
-		" framerate=(fraction)" + std::to_string(framerate) +"/1 ! appsink";
+		" framerate=(fraction)" + std::to_string(framerate) +"/1 ! videoconvert ! queue ! appsink sync=false";
 }
 
 void updateMatrices(){
 
 	cv::Point2f center((input_width)*0.5f, (input_height)*0.5f);
-    cv::Mat newRotMat = cv::getRotationMatrix2D(center, r, 1.0 + z);
+    cv::Mat newRotMat = cv::getRotationMatrix2D(center, r, 1.0);
 
 	float rr = r*PI/180.0f;
 	float a = cos(rr);
@@ -148,11 +144,11 @@ void updateMatrices(){
 
 	cameraMatrix.at<float>(0, 0) = input_width * newRotMat.at<double>(0,0);
 	cameraMatrix.at<float>(0, 1) = input_width * newRotMat.at<double>(0,1);
-	cameraMatrix.at<float>(0, 2) = input_width * (0.75f + z);
+	cameraMatrix.at<float>(0, 2) = input_width * (1.0f - 2.0f*(0.5f-zx));
 
  	cameraMatrix.at<float>(1, 0) = input_height * newRotMat.at<double>(1,0);
 	cameraMatrix.at<float>(1, 1) = input_height * newRotMat.at<double>(1,1);
-	cameraMatrix.at<float>(1, 2) = input_height * (0.75f + z);
+	cameraMatrix.at<float>(1, 2) = input_height * (1.0f - 2.0f*(0.5f-zy));
 
 	cameraMatrix.at<float>(2, 0) = 0.0f;
 	cameraMatrix.at<float>(2, 1) = 0.0f;
@@ -176,7 +172,26 @@ void updateMatrices(){
 
 	roi_rect = cv::Rect(x,y,w,h);
 
-    dilate_element = cv::getStructuringElement( cv::MORPH_ELLIPSE, cv::Size( (int)(10.0f*d) + 1, (int)(10.0f*d) + 1 ), cv::Point( (int)(10.0f*d), (int)(10.0f*d)) );
+	int ds = (int)(d*6);
+	int es = (int)(e*6);
+
+    erode_element = cv::getStructuringElement( cv::MORPH_RECT, cv::Size( 1+es*2, 1+es*2 ), cv::Point(es,es));
+    dilate_element = cv::getStructuringElement( cv::MORPH_ELLIPSE, cv::Size( 1+ds*2, 1+ds*2 ), cv::Point(ds,ds));
+
+	//params.minThreshold = 10;
+	//params.maxThreshold = 200;
+	params.minDistBetweenBlobs = 50.0f;
+	params.filterByInertia = false;
+	//params.minInertiaRatio = 0.01;
+	params.filterByConvexity = false;
+	//params.minConvexity = 0.87;
+	params.filterByColor = false;
+	params.filterByCircularity = false;
+	//params.minCircularity = 0.1;
+	params.filterByArea = true;
+	params.minArea = 100.0f;
+	params.maxArea = 1000.0f;
+
 
 }
 
@@ -187,64 +202,133 @@ void callback_ROI_Y( int i, void* ) { y = i; updateMatrices(); }
 void callback_ROI_W( int i, void* ) { w = i; updateMatrices(); }
 void callback_ROI_H( int i, void* ) { h = i; updateMatrices(); }
 void callback_ROI_R( int i, void* ) { r = (float)i; updateMatrices(); }
-void callback_ROI_Z( int i, void* ) { z = i/1000.0f - .5f; updateMatrices(); }
+void callback_ROI_Q( int i, void* ) { q = i/1000.0f; updateMatrices(); }
+void callback_ROI_ZX( int i, void* ) { zx = i/1000.0f - .5f; updateMatrices(); }
+void callback_ROI_ZY( int i, void* ) { zy = i/1000.0f - .5f; updateMatrices(); }
 void callback_CAM_K1( int i, void* ) { k1 = i/1000.0f - .5f; updateMatrices(); }
 void callback_CAM_K2( int i, void* ) { k2 = i/1000.0f - .5f; updateMatrices(); }
 void callback_CAM_K3( int i, void* ) { k3 = i/1000.0f - .5f; updateMatrices(); }
 void callback_CAM_P1( int i, void* ) { p1 = i/1000.0f - .5f; updateMatrices(); }
 void callback_CAM_P2( int i, void* ) { p2 = i/1000.0f - .5f; updateMatrices(); }
-void callback_E_CUT( int i, void* ) { cut = i/1000.0f - .5f; updateMatrices(); }
-void callback_E_D( int i, void* ) { d = i/1000.0f - .5f; updateMatrices(); }
-void callback_E_E( int i, void* ) { e = i/1000.0f - .5f; updateMatrices(); }
+void callback_E_CUT( int i, void* ) { cut = i/1000.0f; updateMatrices(); }
+void callback_E_D( int i, void* ) { d = i/1000.0f; updateMatrices(); }
+void callback_E_E( int i, void* ) { e = i/1000.0f; updateMatrices(); }
+void callback_E_C( int i, void* ) { c = i/1000.0f; updateMatrices(); }
 
 
 void callback( int i, void* ) {}
 
+std::vector<int> trackbarPos = {0, 0, input_width, input_height, 180, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500};
+
+std::string statefilePath("trackbarStates.dat");
+
+void saveTrackbars(){
+	std::cout << "Saving Trackbars" << std::endl;
+
+	//std::ofstream file(statefilePath, std::ios::out | std::ofstream::binary);
+	//std::copy(trackbarPos.begin(), trackbarPos.end(), std::ostreambuf_iterator<char>(file));
+    // Write out a list to a disk file
+
+	trackbarPos.at(0) = cv::getTrackbarPos("ROI_X", "adjustments");
+	trackbarPos.at(1) = cv::getTrackbarPos("ROI_Y", "adjustments");
+	trackbarPos.at(2) = cv::getTrackbarPos("ROI_W", "adjustments");
+	trackbarPos.at(3) = cv::getTrackbarPos("ROI_H", "adjustments");
+	trackbarPos.at(4) = cv::getTrackbarPos("r", "adjustments");
+	trackbarPos.at(5) = cv::getTrackbarPos("q", "adjustments");
+	trackbarPos.at(6) = cv::getTrackbarPos("zx", "adjustments");
+	trackbarPos.at(7) = cv::getTrackbarPos("zy", "adjustments");
+	trackbarPos.at(8) = cv::getTrackbarPos("k1", "adjustments");
+	trackbarPos.at(9) = cv::getTrackbarPos("k2", "adjustments");
+	trackbarPos.at(10) = cv::getTrackbarPos("k3", "adjustments");
+	trackbarPos.at(11) = cv::getTrackbarPos("p1", "adjustments");
+	trackbarPos.at(12) = cv::getTrackbarPos("p2", "adjustments");
+	trackbarPos.at(13) = cv::getTrackbarPos("cut", "adjustments");
+	trackbarPos.at(14) = cv::getTrackbarPos("d", "adjustments");
+	trackbarPos.at(15) = cv::getTrackbarPos("e", "adjustments");
+	trackbarPos.at(16) = cv::getTrackbarPos("c", "adjustments");
+
+    std::ofstream os (statefilePath, std::ios::binary);
+    int s = trackbarPos.size();
+    os.write((const char*)&s, sizeof(s));
+    os.write((const char*)&trackbarPos[0], s * sizeof(int));
+    os.close();
+}
+
+void loadTrackbars(){
+	std::cout << "Loadint Trackbars" << std::endl;
+
+	//std::ifstream file(statefilePath, std::ios::in | std::ifstream::binary);
+	//std::istreambuf_iterator iter(file);
+	//std::copy(iter.begin(), iter.end(), std::back_inserter(trackbarPos));
+
+    std::ifstream is(statefilePath, std::ios::binary);
+
+	if(!is.good()) return;
+
+    int s;
+    is.read((char*)&s, sizeof(s));
+    trackbarPos.resize(s);
+
+     // Is it safe to read a whole array of structures directly into the vector?
+    is.read((char*)&trackbarPos[0], s * sizeof(int));
+}
+
 void makeGUI(){
+
+	loadTrackbars();
+
     cv::namedWindow( "adjustments", cv::WINDOW_AUTOSIZE );
 
-	cv::createTrackbar("ROI X", "adjustments", NULL, input_width, callback_ROI_X);
+	cv::createTrackbar("ROI_X", "adjustments", NULL, input_width, callback_ROI_X);
+	cv::setTrackbarPos("ROI_X", "adjustments", trackbarPos.at(0));
 
-	cv::createTrackbar("ROI Y", "adjustments", NULL, input_height, callback_ROI_Y);
+	cv::createTrackbar("ROI_Y", "adjustments", NULL, input_height, callback_ROI_Y);
+	cv::setTrackbarPos("ROI_Y", "adjustments", trackbarPos.at(1));
 
-	cv::createTrackbar("ROI W", "adjustments", NULL, input_width, callback_ROI_W);
-	cv::setTrackbarPos("ROI W", "adjustments", input_width);
+	cv::createTrackbar("ROI_W", "adjustments", NULL, input_width, callback_ROI_W);
+	cv::setTrackbarPos("ROI_W", "adjustments", trackbarPos.at(2));
 
-	cv::createTrackbar("ROI H", "adjustments", NULL, input_height, callback_ROI_H);
-	cv::setTrackbarPos("ROI H", "adjustments", input_height);
+	cv::createTrackbar("ROI_H", "adjustments", NULL, input_height, callback_ROI_H);
+	cv::setTrackbarPos("ROI_H", "adjustments", trackbarPos.at(3));
 
 	cv::createTrackbar("r", "adjustments", NULL, 360, callback_ROI_R);
+	cv::setTrackbarPos("r", "adjustments", trackbarPos.at(4));
 
-	cv::createTrackbar("z", "adjustments", NULL, 1000, callback_ROI_Z);
-	cv::setTrackbarPos("z", "adjustments", 500);
+	cv::createTrackbar("q", "adjustments", NULL, 1000, callback_ROI_Q);
+	cv::setTrackbarPos("q", "adjustments", trackbarPos.at(5));
+
+	cv::createTrackbar("zx", "adjustments", NULL, 1000, callback_ROI_ZX);
+	cv::setTrackbarPos("zx", "adjustments", trackbarPos.at(6));
+
+	cv::createTrackbar("zy", "adjustments", NULL, 1000, callback_ROI_ZY);
+	cv::setTrackbarPos("zy", "adjustments", trackbarPos.at(7));
 
 	cv::createTrackbar("k1", "adjustments", NULL, 1000, callback_CAM_K1);
-	cv::setTrackbarPos("k1", "adjustments", 500);
+	cv::setTrackbarPos("k1", "adjustments", trackbarPos.at(8));
 
 	cv::createTrackbar("k2", "adjustments", NULL, 1000, callback_CAM_K2);
-	cv::setTrackbarPos("k2", "adjustments", 500);
+	cv::setTrackbarPos("k2", "adjustments", trackbarPos.at(9));
 
 	cv::createTrackbar("k3", "adjustments", NULL, 1000, callback_CAM_K3);
-	cv::setTrackbarPos("k3", "adjustments", 500);
+	cv::setTrackbarPos("k3", "adjustments", trackbarPos.at(10));
 
 	cv::createTrackbar("p1", "adjustments", NULL, 1000, callback_CAM_P1);
-	cv::setTrackbarPos("p1", "adjustments", 500);
+	cv::setTrackbarPos("p1", "adjustments", trackbarPos.at(11));
 
 	cv::createTrackbar("p2", "adjustments", NULL, 1000, callback_CAM_P2);
-	cv::setTrackbarPos("p2", "adjustments", 500);
-
-	cv::createTrackbar("p2", "adjustments", NULL, 1000, callback_CAM_P2);
-	cv::setTrackbarPos("p2", "adjustments", 500);
+	cv::setTrackbarPos("p2", "adjustments", trackbarPos.at(12));
 
 	cv::createTrackbar("cut", "adjustments", NULL, 1000, callback_E_CUT);
-	cv::setTrackbarPos("cut", "adjustments", 500);
+	cv::setTrackbarPos("cut", "adjustments", trackbarPos.at(13));
 
 	cv::createTrackbar("d", "adjustments", NULL, 1000, callback_E_D);
-	cv::setTrackbarPos("d", "adjustments", 500);
+	cv::setTrackbarPos("d", "adjustments", trackbarPos.at(14));
 
 	cv::createTrackbar("e", "adjustments", NULL, 1000, callback_E_E);
-	cv::setTrackbarPos("e", "adjustments", 500);
+	cv::setTrackbarPos("e", "adjustments", trackbarPos.at(15));
 
+	cv::createTrackbar("c", "adjustments", NULL, 1000, callback_E_C);
+	cv::setTrackbarPos("c", "adjustments", trackbarPos.at(16));
 
     cv::resizeWindow("adjustments", 240, 480);
     cv::moveWindow("adjustments", 240, 0);
@@ -286,25 +370,21 @@ int main(int argc, char *argv[]) {
 
 	while(cap.isOpened()){
 
+//        std::cout << "Capture?" << std::endl;
+
     	if (!cap.read(cam_input)) {
             std::cout << "Capture read error" << std::endl;
             continue;
         }
 
-		cv::cvtColor(cam_input, cam_procbuf_1, cv::COLOR_BGR2GRAY);
-		cv::rectangle(cam_input, roi_rect, cv::Scalar(255, 0, 255));
+//        std::cout << "Capture!" << std::endl;
 
-		cv::remap(cam_procbuf_1, cam_procbuf_2, mapx, mapy, cv::INTER_LINEAR, cv::BORDER_REPLICATE);
+		cv::cvtColor(cam_input, cam_gray, cv::COLOR_BGR2GRAY);
+		cv::remap(cam_gray, cam_remapped, mapx, mapy, cv::INTER_LINEAR, cv::BORDER_REPLICATE);
+		cv::rectangle(cam_remapped, roi_rect, cv::Scalar(255));
 //		cv::cvtColor(cam_input(roi_rect), cam_procbuf_1, cv::COLOR_BGR2GRAY );
 
-		cam_procbuf_2 = cam_procbuf_2(roi_rect);
-
-
-		// Detect blobs.
-		//detector->detect( cam_processed, keypoints );
-		// Draw detected blobs as red circles.
-		// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
-		//cv::drawKeypoints( cam_processed, keypoints, cam_processed, eye_color, cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+		cam_procbuf_1 = cam_remapped(roi_rect);
 
 
 /*
@@ -318,11 +398,21 @@ int main(int argc, char *argv[]) {
         static int posY = moment01/area;
 */
 
-		cv::threshold(cam_procbuf_2, cam_procbuf_5, (int)(e*254), 255, cv::THRESH_BINARY_INV);
-//		cv::Canny(cam_procbuf_1, cam_procbuf_5, (int)(cut*254), 254, 3);
-//		cv::dilate(cam_procbuf_2, cam_procbuf_5, dilate_element);
+		cv::threshold(cam_procbuf_1, cam_procbuf_2, (int)(cut*254), 255, cv::THRESH_BINARY_INV);
+//		cv::Canny(cam_procbuf_1, cam_procbuf_2, (int)(cut*254), 254, 3);
 
-//		cv::accumulateWeighted(cam_procbuf_1, eye_buffer_R, 0.2);
+//		cv::erode(cam_procbuf_2, cam_procbuf_3, erode_element, cv::Point(-1,-1), (int)(e*10));
+//		cv::dilate(cam_procbuf_3, cam_procbuf_5, dilate_element, cv::Point(-1,-1), (int)(d*10));
+
+		cv::erode(cam_procbuf_2, cam_procbuf_3, erode_element);
+		cv::dilate(cam_procbuf_3, cam_procbuf_5, dilate_element);
+
+//		cv::accumulateWeighted(cam_procbuf_5, eye_buffer_R, 0.2);
+
+		// Detect blobs.
+		detector->detect( cam_procbuf_5, keypoints );
+		cv::drawKeypoints( cam_procbuf_5, keypoints, eye_buffer_R, eye_color, cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+		cv::drawKeypoints( cam_procbuf_5, keypoints, eye_buffer_L, eye_color, cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
 		cv::resize(eye_buffer_R, eye_out_R, cv::Size(EMAT_W, EMAT_H), 0, 0, cv::INTER_NEAREST);
 		cv::resize(eye_buffer_L, eye_out_L, cv::Size(EMAT_W, EMAT_H), 0, 0, cv::INTER_NEAREST);
@@ -349,9 +439,9 @@ int main(int argc, char *argv[]) {
 //		cv::imshow("eye_output_L", eye_out_L);
 //		cv::imshow("eye_output_R", eye_out_R);
 
-		cv::hconcat(eye_out_L, eye_out_R, eye_out_preview);
-		cv::imshow("adjustments", eye_out_preview);
-		cv::imshow("cam_input", cam_input);
+//		cv::hconcat(eye_out_L, eye_out_R, eye_out_preview);
+//		cv::imshow("adjustments", eye_out_preview);
+		cv::imshow("cam_input", cam_remapped);
 		cv::imshow("cam_buf", cam_procbuf_5);
 
 		//- PANEL OUTPUT -----------------------------------------------------------------
@@ -379,9 +469,11 @@ int main(int argc, char *argv[]) {
 		driver_write(0b11111111111111111111111111111111);
 		usleep(1000);
 		//--------------------------------------------------------------------------------*/
-		int key = cv::waitKey(10);
+
+		int key = cv::waitKey(16);
 		if(key == 27 || key == 81) break;
 		switch(key){
+			case 115: saveTrackbars(); break;
 			case 109:
 				mode++;
 				if(mode > 5) mode = 0;
